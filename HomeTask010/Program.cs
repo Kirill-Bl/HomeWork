6debbbae97060cc20008c44209a1197739fbc0bc
Fin <fin@fin>
2022-12-07 17:47:32 +0300

    HomeWork Seminar II C#

---
+++ HomeTask010/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+Console.WriteLine("Отобразит вторую цифру введеноготрех значного числа");
+Console.Write("Input N = ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 100 || n > 999)
+{
+    Console.WriteLine("Вы не ввели ТРЕХЗНАЧНОЕ число");
+    Console.Write("Input N = ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+n = n % 100;
+n = (n - (n % 10)) / 10;
+Console.Write(n);
---
+++ HomeTask015/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Clear();
+Console.WriteLine("Введите номер дня недели, от 1 - 7");
+Console.Write("Input N = ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 1 || n > 7)
+{
+    Console.WriteLine("Вы не ввели номер дня, от 1 до 7");
+    Console.Write("Input N = ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+if (n == 6 || n == 7)
+    Console.WriteLine("Выходной");
+else
+    Console.WriteLine("Будни");
