0a5d3d5d1ab347193de20232d2b61ff1476676b8
Kirill-Bl <118842105+Kirill-Bl@users.noreply.github.com>
2022-12-08 10:11:48 +0300

    Revert "Home tasks"

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Input Name");
+string  username = Console.ReadLine();
+Console.Write("Hello,");
+Console.WriteLine(username);
+
---
+++ ExampleTask014/Program.cs
@@ -0,0 +1,13 @@
+﻿// Вводим число, проверяем кратно-ли оно одновременно 7 и 23,
+// Если да, выводит "кратно"
+//      нет, выводит "не кратно"
+
+Console.Clear();
+Console.WriteLine("Определения одновременной кратности чисела N1 числам 7 и 23");
+Console.Write("Input N1 = ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n % 7 == 0 && n % 23 == 0)
+    Console.Write("Кратно");
+else
+    Console.Write("Не кратно");
+
