a9e624335bc619c668b2d48417d3675bad25d2dd
Fin <fin@fin.com>
2022-12-27 15:40:02 +0300

    HomeWork Sem IV

--- HomeTask025/Program.cs
+++ HomeTask025/Program.cs
@@ -9,7 +9,7 @@ Console.Write("Input A = ");
 double a = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Input B = ");
-double b = Convert.ToInt32(Console.ReadLine());
+double b = Convert.ToInt32(Console.ReadLine()); // Проверку на 0 и отрицательные и т.д. прописывать не стал
 double result = a;
 int i = 1;
 while (i < b)
